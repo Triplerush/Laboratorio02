155248c6fbfd3401d4772ecc28f93021542d8a2b
Triplerush <fcanal@unsa.edu.pe>
2022-05-13 09:24:44 -0500

    Correcion del nombre del archivo

---
+++ ejercicio3/script.js
@@ -0,0 +1,7 @@
+function calcularDiasFaltantes(){
+	const fechaActual = new Date();
+	const dia = fechaActual.getDate();
+	const mes = fechaActual.getMonth();
+	
+	console.log(fechaActual + "\n"+dia +"\n"+ mes); 
+}
