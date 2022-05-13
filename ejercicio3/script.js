efbf2abe71be7d65ab8d7c7e390c72702eafa164
Triplerush <fcanal@unsa.edu.pe>
2022-05-13 09:28:37 -0500

    Calculando el número de dias antes del 15 de agosto

--- ejercicio3/script.js
+++ ejercicio3/script.js
@@ -2,6 +2,15 @@ function calcularDiasFaltantes(){
 	const fechaActual = new Date();
 	const dia = fechaActual.getDate();
 	const mes = fechaActual.getMonth();
+	const diasMeses = [31,28,31,30,31,30,31,31,30,31,30,31];
+
+	 let diasFaltantes = 0;
+
+	for(let i = mes; i < 7; i++){
+	  diasFaltantes += diasMeses[i];
+	}
 	
-	console.log(fechaActual + "\n"+dia +"\n"+ mes); 
+	diasFaltantes += 15 - dia;
+
+	console.log(diasFaltantes);
 }
