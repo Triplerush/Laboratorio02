d8a40bef12bf9760e2e8cb0672931db33f039ca5
Triplerush <fcanal@unsa.edu.pe>
2022-05-13 09:45:20 -0500

    Calculando el número desde cualquier fecha

--- ejercicio3/script.js
+++ ejercicio3/script.js
@@ -1,18 +1,29 @@
 function calcularDiasFaltantes(){
 	const fechaActual = new Date();
-	const dia = fechaActual.getDate();
-	const mes = fechaActual.getMonth();
+	const dia = 16;
+	let mes = 7;
 	const diasMeses = [31,28,31,30,31,30,31,31,30,31,30,31];
 
-	 let diasFaltantes = 0;
+	let diasFaltantes = 0;
+	let diasExtra;
+
+	if(mes == 7 && dia> 15){
+		mes = 8;
+    diasExtra = 31 + 15 - dia;
+  }else{
+ 		diasExtra = 15 - dia;
+ 	}
 
 	for(let i = mes; i != 7; i++){
+		console.log(diasMeses[i]);
 	  diasFaltantes += diasMeses[i];
 		if(i == 11){
-			i = 0;
+			i = -1;
 		}
 	}
+	console.log(diasFaltantes)
+	console.log(diasExtra)
 	
-	diasFaltantes += 15 - dia;
+	diasFaltantes += diasExtra;
 	document.getElementById('resultado').innerHTML = "Faltan " +diasFaltantes + " para el día de Arequipa";
 }
