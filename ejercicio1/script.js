d63de9254882ecae43d4bee31c389f915243a25a
Triplerush <fcanal@unsa.edu.pe>
2022-05-13 11:39:59 -0500

    La funcion ahora calcula el numero de dias pasados desde el año 1990 y de acuerdo a eso calcula el dia de la semana

--- ejercicio1/script.js
+++ ejercicio1/script.js
@@ -1,15 +1,38 @@
 let fechaActual = new Date();
 let dia = fechaActual.getDate();
+let mes = fechaActual.getMonth();
+let year = fechaActual.getFullYear();
+diaSemana(dia,mes,year);
 
-diaSemana(dia);
-
-
-function diaSemana(numeroDia){
+function diaSemana(dia,mes,year){
+  const yearBase = 1990;
+  const diasMeses = [31,28,31,30,31,30,31,31,30,31,30,31];
+  let diasPasados = 0; 
   let diasSemana = ['Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado']
-  if(numeroDia > 31) 
-    console.log('Número de dia equivocado');
-  else
-    console.log(diasSemana[numeroDia%7]);
+  
+  for(let i = yearBase; i < year ; i++){
+    diasPasados += 365;
+    if(i % 4 ==0){
+      diasPasados++;
+    }
+  }
+
+
+  if(year % 4==0){
+    diasMeses[1] = 29;
+  }else
+    diasMeses[1] = 28;
+
+  
+  for(let i = 0; i< mes ; i++){
+    diasPasados +=  diasMeses[i];
+  }
+
+  diasPasados += dia;
+
+  const indiceSemana = diasPasados % 7;
+
+  console.log(diasSemana[indiceSemana%7]);
 } 
 
 
