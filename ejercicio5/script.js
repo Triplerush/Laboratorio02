689100024d1eb62e9ab6b9767cac25295f76a44f
Triplerush <fcanal@unsa.edu.pe>
2022-05-13 09:58:30 -0500

    Agregue la funcion para crear la tabla con números aleatorios segun el ancho y el largo

--- ejercicio5/script.js
+++ ejercicio5/script.js
@@ -1,3 +1,22 @@
+function crearTabla(ancho,largo){
+	let numRandom;
+	let html = '<table border="2">';
+
+	for(let i=0;i<ancho;i++){
+		html += '\n  <tr>';
+		for(let j=0;j<largo;j++){
+			numRandom = Math.ceil(Math.random()*(100));
+		  html += '\n    <th>' + numRandom+ '</th>';
+		}
+
+		html += '\n  </tr>';
+	}
+
+	html +='\n</table>\n<button type="button" onclick="">Calcular</button>'
+
+	return html;
+}
+
 document.addEventListener('DOMContentLoaded', function(){
 	const ancho = document.querySelector('#ancho');
 	const largo= document.querySelector('#largo');
@@ -19,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function(){
 	};
 
 	document.querySelector('#form').onsubmit = () =>{
-		document.getElementById('tabla').innerHTML = "La tabla va aqui"; 
+		document.getElementById('tabla').innerHTML = crearTabla(ancho.value,largo.value); 
 
 		return false;
 	}
