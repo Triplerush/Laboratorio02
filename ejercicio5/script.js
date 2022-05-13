ef9e7022948775e85db5c3c8cffee709659d2a07
Triplerush <fcanal@unsa.edu.pe>
2022-05-13 10:05:47 -0500

    Captura de la suma de datos para luego ser mostrada al apretar el boton

--- ejercicio5/script.js
+++ ejercicio5/script.js
@@ -1,20 +1,27 @@
 function crearTabla(ancho,largo){
 	let numRandom;
+	let sumaTotal = 0;
 	let html = '<table border="2">';
 
 	for(let i=0;i<ancho;i++){
 		html += '\n  <tr>';
 		for(let j=0;j<largo;j++){
 			numRandom = Math.ceil(Math.random()*(100));
+			sumaTotal += numRandom;
 		  html += '\n    <th>' + numRandom+ '</th>';
 		}
 
 		html += '\n  </tr>';
 	}
 
-	html +='\n</table>\n<button type="button" onclick="">Calcular</button>'
+	html +='\n</table>\n<button type="button" onclick=mostrarSuma('+sumaTotal+')>Calcular</button>'
 
+	document.getElementById('resultado').innerHTML = '';
 	return html;
+}
+
+function mostrarSuma(sumaTotal){
+	document.getElementById('resultado').innerHTML = "La suma total es: " + sumaTotal;
 }
 
 document.addEventListener('DOMContentLoaded', function(){
