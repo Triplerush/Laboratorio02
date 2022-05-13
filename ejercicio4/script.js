6b9dba32e16241ef6f28b98eb387e04a6d401fd9
Triplerush <fcanal@unsa.edu.pe>
2022-05-13 11:13:03 -0500

    Creacion de la funcion para sacar los guiones

--- ejercicio4/script.js
+++ ejercicio4/script.js
@@ -1,3 +1,27 @@
+function quitarGuiones(texto){
+	let textoSinGuiones = '';
+	let letraActual;
+	let signoInicial = 0;
+	let signoFinal = true;
+
+	for(let i = 0; i<texto.length;i++){
+		letraActual = texto.charAt(i);
+
+		if(letraActual == '/'){
+			signoInicial++;
+			continue;
+		}
+		if(letraActual == '?'){
+			signoFinal = false;
+		}
+		if(letraActual != '-' && signoInicial == 3 && signoFinal){
+			textoSinGuiones += letraActual;
+		}
+	}
+
+	return textoSinGuiones;
+}
+
 document.addEventListener('DOMContentLoaded', function(){
 	const texto = document.querySelector('#texto');
 	const boton = document.querySelector('#enviar');
